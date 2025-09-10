b8fef22cf3f827c0730637a9406bbe9322178d0a
gaugaura <gaurav.2.gaurav@nokia.com>
2025-09-10 09:45:54 +0530

    adding data transformation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -24,3 +24,25 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(filepath:str, array: np.array):
+    """ save numpy array data to a file 
+    filepath:str = location of file to save 
+    array: np.array = data to save 
+    """
+    try:
+        dir_path = os.path.dirname(filepath)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(filepath,"wb") as file_object:
+            np.save(file_object,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def save_object(file_path:str,obj:object) -> None:
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_object:
+            pickle.dump(file_object,obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
